32b93a8ea2ed5c81721d1f2aba6dc7bd1a561ec3
Carmen Owen <doctorsout@yahoo.com>
2018-12-17 21:32:58 -0500

    Ready to start section 3 lecture 47

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ListComponent } from './shopping-list/list/list.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ListComponent,
+    ListEditComponent,
+    IngredientComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
